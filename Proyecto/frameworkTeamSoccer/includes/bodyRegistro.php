62c767e337fd2c21d679fc9a27bbf0fe31f6f74f
DWES <DWES@10.0.2.15>
2019-11-22 14:03:05 +0100

    parteEnrique

--- Proyecto/frameworkTeamSoccer/includes/bodyRegistro.php
+++ Proyecto/frameworkTeamSoccer/includes/bodyRegistro.php
@@ -1,13 +1,52 @@
-
 <div class="container">
-<form class="form-register" action="acceso.php">
-    <h4>Formulario de registro</h4>
-  <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su nombre de usuario">
-  <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su correo">
-  <input class="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingrese su contraseña">
-  <input class="controls" type="password" name="contraseña2" id="contraseña2" placeholder="Repita la contraseña">
-  <input type="checkbox">Estoy de acuerdo con <a href="#"> Terminos y Condiciones</a>  
-  <input class="boton" type="submit" name="registrar" value="Registrar">
-  <p><a href="acceso.php">Ya tengo cuenta</a></p>
-</form>
+	<div class="row text-center">
+	    <div class="col-md-6 offset-md-3">
+	        <div class="card">
+	            <div class="card-body">
+	                <div>
+                            <img src="../iconos/contacto.png">
+	                </div>
+	                <div>
+	                    <h4>Sign Up</h4>
+	                </div>
+	                <div class="login-form mt-4">
+	                    <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                              <input name="nombre" placeholder="Nombre" class="form-control" type="text">
+                            </div>
+                            <div class="form-group col-md-12">
+                              <input type="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group col-md-12">
+                              <input type="pass" class="form-control"  placeholder="Contraseña">
+                            </div>
+                             <div class="form-group col-md-12">
+                              <input type="pass" class="form-control"  placeholder="Repetir contraseña">
+                            </div>
+                          </div>
+                         <div class="form-row">
+                        <div class="form-group">
+                                <div class="form-check">
+                                  <input class="form-check-input" type="checkbox" value="">
+                                  <label class="form-check-label" for="updatecheck1">
+                                    <small>Acepto todos los <a href="#">terminos y condiciones </a></small>
+                                    
+                                  </label>
+                                </div>
+                              </div>
+                    </div>                        
+                        
+                        <div class="form-row">
+                            <button type="button" class="btn btn-danger btn-block">Enviar</button>
+                        </div>
+                    </form>
+	                </div>
+	                <div class="text-right mt-2">
+	                    <a href="acceso.php">Ya tengo una cuenta </a>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
 </div>
