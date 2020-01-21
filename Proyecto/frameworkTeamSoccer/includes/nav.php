848cd1e4cc02ae5ac83958c47d1f30db5a4cd2d5
DWES <DWES@10.0.2.15>
2020-01-21 10:12:28 +0100

    hola k ase

--- Proyecto/frameworkTeamSoccer/includes/nav.php
+++ Proyecto/frameworkTeamSoccer/includes/nav.php
@@ -63,6 +63,71 @@
 
 
             </div>
+                <div class="container">
+
+        <a class="navbar-brand" href="#">
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+            &#9776;
+        </button>
+        <div class="collapse navbar-collapse" id="exCollapsingNavbar">
+           
+            <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
+                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
+                <li id="dropdownLoginLI" class="dropdown order-1">
+                    <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
+                    <ul class="dropdown-menu dropdown-menu-right mt-2">
+                       <li class="px-3 py-2">
+                           <form class="form" role="form">
+                                <div class="form-group">
+                                    <input id="emailInput" placeholder="Email" class="form-control form-control-sm" type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                                </div>
+                                <div class="text-center">
+                                    <small>or</small>
+                                </div>
+
+                                <div class="form-group">
+                                    <button id="googleSignInBtn" class="btn-google">SIGN IN WITH GOOGLE</button>
+                                </div>
+                               
+                                <div class="form-group text-center">
+                                    <small><a href="#" data-toggle="modal" data-target="#modalPassword">Forgot password?</a></small>
+                                </div>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                <li id="dropdownLogoutLI" class="dropdown order-1" style="display:none;">
+                    <button type="button" id="dropdownLogoutMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Name: <span class="caret"></span></button>
+                    <ul class="dropdown-menu dropdown-menu-right mt-2">
+                       <li class="px-3 py-2">
+                           <form class="form" role="form">
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Edit profile</button>
+                                </div>
+                                <div class="text-center">
+                                    <small>or</small>
+                                </div>
+
+                                <div class="form-group">
+                                    <button id="googleSignoutBtn" onclick="signOut()" class="btn-google">SIGN OUT</button>
+                                </div>
+                               
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+
         </div>
     </nav>
 
